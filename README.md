3c93e3b333c38813196dd3f41255f88777eaede8
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2020-06-11 17:29:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# yStnadard Toolbox
+
+無料WordPressテーマ「yStandard」用機能拡張プラグイン
+
+## 更新履歴
+
+### v0.0.1
+- [追加] β版作成
+
+## Third-party resources
+
+### Plugin Update Checker
+
+License: MIT
+Source : <https://github.com/YahnisElsts/plugin-update-checker>
+
+### React FontIconPicker
+
+License: MIT
+Source : <https://github.com/fontIconPicker/react-fonticonpicker>
+
+### IntersectionObserver
+
+License: W3C Software and Document License
+Source : <https://github.com/w3c/IntersectionObserver>
+
+### Feather
+
+License: MIT  
+Source : <https://github.com/feathericons/feather>
+
+### React Feather Icons
+
+License: MIT  
+Source : <https://github.com/feathericons/react-feather>
+
+### Simple Icons
+
+License: CC0 - 1.0  
+Source : <https://github.com/simple-icons/simple-icons>
+
+### Vue.js
+
+License: MIT  
+Source : <https://github.com/vuejs/vue>
+
+### vue-color
+
+License: MIT  
+Source : <https://github.com/xiaokaike/vue-color>
+
+### Lato
+
+License: Open Font License.  
+Source : <https://fonts.google.com/specimen/Lato>
---
+++ ystandard-toolbox.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Plugin Name: yStandard Toolbox
+ * Plugin URI: https://wp-ystandard.com/
+ * Description: 無料WordPressテーマ「yStandard」用機能拡張プラグイン.
+ * Version: 0.0.1
+ * Author: yosiakatsuki
+ * Author URI: https://yosiakatsuki.net
+ * License: GPL-2.0 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ystandard-toolbox
+ * Domain Path: languages
+ *
+ * @package yStandard_toolbox
+ */
+
+/*
+	Copyright (c) 2019 Yoshiaki Ogata (https://yosiakatsuki.net/)
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * [yStandard]確認
+ */
+$theme = wp_get_theme( get_template() );
+if ( 'ystandard' !== $theme->template ) {
+	return;
+}
