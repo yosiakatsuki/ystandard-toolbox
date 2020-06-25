bdbf6d3fa338135a33db8b1a1ed9fe28a7fd2161
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2020-06-25 22:49:49 +0900

    set up github workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const autoprefixer = require( 'autoprefixer' );
 const mqpacker = require( 'css-mqpacker' );
 const cssdeclsort = require( 'css-declaration-sorter' );
 const cssnano = require( 'cssnano' );
+const gulpZip = require( 'gulp-zip' );
+const del = require( 'del' );
 
 const postcssPlugins = [
 	autoprefixer( {
@@ -28,11 +30,81 @@ function sass() {
 		.pipe( dest( './css' ) );
 }
 
+function cleanFiles( cb ) {
+	return del(
+		[
+			'./ystandard-toolbox',
+			'./build'
+		],
+		cb );
+}
+
+/**
+ * 必要ファイルのコピー
+ */
+function copyProductionFiles() {
+	return src(
+		[
+			'**',
+			'!.gitignore',
+			'!.travis.yml',
+			'!node_modules',
+			'!node_modules/**',
+			'!gulpfile.js',
+			'!package.json',
+			'!package-lock.json',
+			'!webpack.config.js',
+			'!ystandard-toolbox-webpack-config.js',
+			'!ystandard-toolbox.json',
+			'!phpcs.ruleset.dist',
+			'!phpcs.ruleset.xml',
+			'!phpunit.xml.dist',
+			'!tests',
+			'!tests/**',
+			'!bin',
+			'!bin/**',
+			'!src',
+			'!src/**',
+			'!blocks/**/*.js',
+			'!blocks/**/*.scss',
+			'!.github',
+			'!.github/**',
+			'!build',
+			'!build/**',
+			'!*.zip',
+			'!ystandard-toolbox',
+			'!ystandard-toolbox/**',
+		],
+		{ base: './' }
+	)
+		.pipe( dest( './ystandard-blocks' ) );
+}
+
+/**
+ * Zip
+ */
+function zip() {
+	return src( 'ystandard-toolbox/**', { base: '.' } )
+		.pipe( gulpZip( 'ystandard-toolbox.zip' ) )
+		.pipe( dest( 'build' ) );
+}
+
+function copyJson() {
+	return src( [ 'ystandard-toolbox.json', 'ystandard-toolbox-beta.json' ] )
+		.pipe( dest( 'build' ) );
+}
+
+
+
 function watchFiles() {
+	cleanFiles();
 	sass();
 	watch( './src/sass/**/*.scss', sass );
 }
+
+exports.createDeployFiles = series( cleanFiles, copyProductionFiles, parallel( zip, copyJson ) );
 exports.sass = series( sass );
 exports.watch = series( watchFiles );
+exports.clean = series( cleanFiles );
 
 exports.default = series( watchFiles );
