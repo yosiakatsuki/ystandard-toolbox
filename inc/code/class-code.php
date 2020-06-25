af7df67cf703eecd55e546dd5dadd1a981e47740
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2020-06-25 22:33:40 +0900

    コード挿入機能追加

--- inc/code/class-code.php
+++ inc/code/class-code.php
@@ -25,8 +25,46 @@ class Code {
 	 * Code constructor.
 	 */
 	public function __construct() {
+		add_action( 'wp_head', [ $this, 'add_head' ] );
+		add_action( 'wp_body_open', [ $this, 'add_body_open' ] );
+		add_action( 'wp_footer', [ $this, 'add_footer' ] );
 	}
 
+	/**
+	 * Headに追加
+	 */
+	public function add_head() {
+		$this->echo_code( 'head', 'head_amp' );
+	}
+
+	/*
+	 * Body開始位置に追加
+	 */
+	public function add_body_open() {
+		$this->echo_code( 'body_open', 'body_open_amp' );
+	}
+
+	/*
+	 * Body終了位置に追加
+	 */
+	public function add_footer() {
+		$this->echo_code( 'body_close', 'body_close_amp' );
+	}
+
+	/**
+	 * コード出力
+	 *
+	 * @param string $option     Option Name.
+	 * @param string $option_amp Option Name(AMP).
+	 */
+	private function echo_code( $option, $option_amp ) {
+		$normal = self::get_option( $option );
+		$amp    = self::get_option( $option_amp );
+		$code   = Utility::is_amp() ? $amp : $normal;
+		if ( ! empty( trim( $code ) ) ) {
+			echo trim( $code ) . PHP_EOL;
+		}
+	}
 
 	/**
 	 * 設定取得
