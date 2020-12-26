61b0a6e76f584147a8dd799a99e658366de5ae0d
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2020-12-26 21:41:55 +0900

    タクソノミー設定整理

--- inc/seo/class-seo.php
+++ inc/seo/class-seo.php
@@ -18,4 +18,66 @@ defined( 'ABSPATH' ) || die();
  */
 class SEO {
 
+	public function __construct() {
+		add_action( 'ystdtb_term_edit_form', [ $this, 'term_seo_edit' ], 10, 2 );
+		add_action( 'ystdtb_term_edit_save', [ $this, 'term_seo_save' ]);
+	}
+
+	/**
+	 * タームにSEO設定追加
+	 *
+	 * @param \WP_Term $tag      Current taxonomy term object.
+	 * @param string   $taxonomy Current taxonomy slug.
+	 */
+	public function term_seo_edit( $tag, $taxonomy ) {
+		?>
+		<div class="ystdtb-term-option__section">
+			<h3 class="ystdtb-term-option__title">SEO設定</h3>
+			<table class="form-table" role="presentation">
+				<tr class="form-field">
+					<th scope="row">
+						<label for="ystdtb-seo-title"><?php echo esc_html( '<title>' ); ?>タグ用タイトル</label>
+					</th>
+					<td>
+						<textarea id="ystdtb-seo-title" name="ystdtb-seo-title" rows="2" cols="40"><?php echo esc_textarea( get_term_meta( $tag->term_id, 'ystdtb-seo-title', true ) ); ?></textarea>
+					</td>
+				</tr>
+				<tr class="form-field">
+					<th scope="row">
+						<label for="ystdtb-seo-description">meta description</label>
+					</th>
+					<td>
+						<textarea id="ystdtb-seo-description" name="ystdtb-seo-description" rows="4" cols="40"><?php echo esc_textarea( get_term_meta( $tag->term_id, 'ystdtb-seo-description', true ) ); ?></textarea>
+					</td>
+				</tr>
+			</table>
+		</div>
+		<?php
+	}
+
+	/**
+	 * SEO設定の保存・削除
+	 *
+	 * @param int $term_id Term ID.
+	 */
+	public function term_seo_save( $term_id ) {
+		/**
+		 * タイトルの上書き
+		 */
+		if ( isset( $_POST['ystdtb-seo-title'] ) && ! empty( $_POST['ystdtb-seo-title'] ) ) {
+			update_term_meta( $term_id, 'ystdtb-seo-title', esc_attr( $_POST['ystdtb-seo-title'] ) );
+		} else {
+			delete_term_meta( $term_id, 'ystdtb-seo-title' );
+		}
+		/**
+		 * 説明の上書き
+		 */
+		if ( isset( $_POST['ystdtb-seo-description'] ) && ! empty( $_POST['ystdtb-seo-description'] ) ) {
+			update_term_meta( $term_id, 'ystdtb-seo-description', esc_attr( $_POST['ystdtb-seo-description'] ) );
+		} else {
+			delete_term_meta( $term_id, 'ystdtb-seo-description' );
+		}
+	}
 }
+
+new SEO();
--- inc/taxonomy/class-taxonomy.php
+++ inc/taxonomy/class-taxonomy.php
@@ -160,26 +160,9 @@ class Taxonomy {
 		?>
 		<div class="ystdtb-term-option">
 			<?php wp_nonce_field( self::NONCE_ACTION, self::NONCE_NAME ); ?>
-			<div class="ystdtb-term-option__label">yStandard Toolbox 設定</div>
+			<h2 class="ystdtb-term-option__label">yStandard Toolbox 設定</h2>
 			<div class="ystdtb-term-option__manual"><?php echo Utility::manual_link_inline( 'manual/ystdtb-term-meta' ); ?></div>
-			<table class="form-table" role="presentation">
-				<tr class="form-field">
-					<th scope="row">
-						<label for="ystdtb-seo-title"><?php echo esc_html( '<title>' ); ?>タグ用タイトル</label>
-					</th>
-					<td>
-						<textarea id="ystdtb-seo-title" name="ystdtb-seo-title" rows="2" cols="40"><?php echo esc_textarea( get_term_meta( $tag->term_id, 'ystdtb-seo-title', true ) ); ?></textarea>
-					</td>
-				</tr>
-				<tr class="form-field">
-					<th scope="row">
-						<label for="ystdtb-seo-description">meta description</label>
-					</th>
-					<td>
-						<textarea id="ystdtb-seo-description" name="ystdtb-seo-description" rows="4" cols="40"><?php echo esc_textarea( get_term_meta( $tag->term_id, 'ystdtb-seo-description', true ) ); ?></textarea>
-					</td>
-				</tr>
-			</table>
+			<?php do_action( 'ystdtb_term_edit_form', $tag, $taxonomy ); ?>
 		</div>
 		<?php
 	}
@@ -194,22 +177,8 @@ class Taxonomy {
 		if ( ! Utility::verify_nonce( self::NONCE_NAME, self::NONCE_ACTION ) ) {
 			return;
 		}
-		/**
-		 * タイトルの上書き
-		 */
-		if ( isset( $_POST['ystdtb-seo-title'] ) && ! empty( $_POST['ystdtb-seo-title'] ) ) {
-			update_term_meta( $term_id, 'ystdtb-seo-title', esc_attr( $_POST['ystdtb-seo-title'] ) );
-		} else {
-			delete_term_meta( $term_id, 'ystdtb-seo-title' );
-		}
-		/**
-		 * 説明の上書き
-		 */
-		if ( isset( $_POST['ystdtb-seo-description'] ) && ! empty( $_POST['ystdtb-seo-description'] ) ) {
-			update_term_meta( $term_id, 'ystdtb-seo-description', esc_attr( $_POST['ystdtb-seo-description'] ) );
-		} else {
-			delete_term_meta( $term_id, 'ystdtb-seo-description' );
-		}
+
+		do_action( 'ystdtb_term_edit_save', $term_id );
 	}
 }
 
