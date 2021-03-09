262a0cf4899e97a92be58de41fada59913885cee
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2021-03-09 22:32:26 +0900

    ブロックパターン管理画面並び替え不具合修正

--- inc/block-patterns/class-block-patterns.php
+++ inc/block-patterns/class-block-patterns.php
@@ -224,8 +224,10 @@ class Block_Patterns {
 	public function set_order( $query ) {
 		if ( is_admin() ) {
 			if ( isset( $query->query['post_type'] ) && self::BLOCK_PATTERNS === $query->query['post_type'] ) {
-				$query->set( 'orderby', 'date' );
-				$query->set( 'order', 'DESC' );
+				if ( ! filter_input( INPUT_GET, 'orderby' ) ) {
+					$query->set( 'orderby', 'date' );
+					$query->set( 'order', 'DESC' );
+				}
 			}
 		}
 	}
