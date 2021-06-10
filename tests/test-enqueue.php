c1d11c475370a46eea7c823c9e6c007ca5bd595a
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2021-06-10 09:13:14 +0900

    テスト調整

--- tests/test-enqueue.php
+++ tests/test-enqueue.php
@@ -17,12 +17,24 @@ class EnqueueTest extends WP_UnitTestCase {
 	 * Enqueue script
 	 */
 	public function test_enqueue_script() {
+		add_filter( 'ystdb_get_version', function () {
+			return '100.0.0';
+		} );
 		$enqueue = new Enqueue();
 
 		$this->assertSame( 11, has_action( 'wp_enqueue_scripts', [ $enqueue, 'enqueue_css' ] ) );
 		$this->assertSame( 11, has_action( 'wp_enqueue_scripts', [ $enqueue, 'enqueue_script' ] ) );
 		$this->assertSame( 50, has_action( 'admin_enqueue_scripts', [ $enqueue, 'admin_enqueue_scripts' ] ) );
-		$this->assertSame( 11, has_action( Config::AFTER_ENQUEUE_CSS_HOOK, [ $enqueue, 'enqueue_block_style' ] ) );
+		$this->assertFalse( has_action( Config::AFTER_ENQUEUE_CSS_HOOK, [ $enqueue, 'enqueue_block_style' ] ) );
+
 		$this->assertSame( PHP_INT_MAX, has_action( 'wp_head', [ $enqueue, 'enqueue_no_script_css' ] ) );
+	}
+
+	public function test_enqueue_script_compat() {
+		add_filter( 'ystdb_get_version', function () {
+			return '';
+		} );
+		$enqueue = new Enqueue();
+		$this->assertSame( 11, has_action( Config::AFTER_ENQUEUE_CSS_HOOK, [ $enqueue, 'enqueue_block_style' ] ) );
 	}
 }
