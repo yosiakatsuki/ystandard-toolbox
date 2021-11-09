a9951937fc6da8eb411fdee5788b14b67ce2f8fa
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2021-11-09 11:49:40 +0900

    add BoxControl

---
+++ src/js/component/box-control/index.js
@@ -0,0 +1,10 @@
+import { __experimentalBoxControl as WPBoxControl } from '@wordpress/components';
+
+const BoxControl = ( props ) => {
+
+	return (
+		<WPBoxControl { ...props } />
+	);
+};
+
+export default BoxControl;
