4c3b16da850ede43287fc8e4c53668a09ded7f9c
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2021-11-24 16:50:48 +0900

    スライダー調整

--- blocks/slider/app.js
+++ blocks/slider/app.js
@@ -18,20 +18,20 @@ Swiper.use( [
 
 // eslint-disable-next-line @wordpress/no-global-event-listener
 document.addEventListener( 'DOMContentLoaded', () => {
+	window.ystdtbSlider = [];
 	let sliders = document.querySelectorAll( '.ystdtb-slider__slider' );
 	sliders = Array.prototype.slice.call( sliders, 0 );
 	sliders.forEach( ( element ) => {
 		let slideItem = element.querySelectorAll(
 			'.ystdtb-slider__container > .wp-block-image,' +
-				'.ystdtb-slider__container > .wp-block-video,' +
-				'.ystdtb-slider__container > .wp-block-cover,' +
-				'.ystdtb-slider__container > .ystdtb-slider-item'
+			'.ystdtb-slider__container > .wp-block-video,' +
+			'.ystdtb-slider__container > .wp-block-cover,' +
+			'.ystdtb-slider__container > .ystdtb-slider-item'
 		);
 		slideItem = Array.prototype.slice.call( slideItem, 0 );
 		slideItem.forEach( ( slideItemElement ) => {
 			slideItemElement.classList.add( 'swiper-slide' );
 		} );
-		const selector = element?.id ? `#${ element.id }` : '.swiper';
 		let options = {};
 		if ( element.hasAttribute( 'data-slider-options' ) ) {
 			try {
@@ -42,6 +42,10 @@ document.addEventListener( 'DOMContentLoaded', () => {
 				options = {};
 			}
 		}
-		new Swiper( selector, options );
+		const slider = new Swiper( element, options );
+		window.ystdtbSlider = [
+			...window.ystdtbSlider,
+			slider,
+		]
 	} );
 } );
--- blocks/slider/inspector-controls/advanced/slider-id.js
+++ blocks/slider/inspector-controls/advanced/slider-id.js
@@ -3,17 +3,12 @@ import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 const SliderId = ( props ) => {
-	const { attributes, setAttributes, clientId } = props;
+	const { attributes, setAttributes } = props;
 	const { sliderId } = attributes;
 	const [ disableIdInput, setDisableIdInput ] = useState( true );
 	const handleOnChange = ( value ) => {
 		setAttributes( { sliderId: value } );
 	};
-	if ( undefined === sliderId ) {
-		let newId = clientId.split( '-' );
-		newId = newId.length ? newId[ 0 ] : clientId;
-		setAttributes( { sliderId: newId } );
-	}
 	return (
 		<BaseControl
 			id={ 'sliderId' }
