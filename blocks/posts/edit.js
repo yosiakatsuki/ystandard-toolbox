e2108bf871d3e23313d34171e5fc3039c9fa1ddb
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-03-28 09:25:37 +0900

    投稿一覧ブロック:タクソノミー変更時にターム設定をクリア

--- blocks/posts/edit.js
+++ blocks/posts/edit.js
@@ -1,142 +1,127 @@
-import { __ } from '@wordpress/i18n';
-import { InspectorControls } from '@wordpress/block-editor';
+import { __ } from "@wordpress/i18n";
+import { InspectorControls } from "@wordpress/block-editor";
 import {
-	PanelBody,
-	ToggleControl,
-	RangeControl,
-	Disabled,
-} from '@wordpress/components';
-import ServerSideRender from '@wordpress/server-side-render';
+  PanelBody,
+  ToggleControl,
+  RangeControl,
+  Disabled,
+} from "@wordpress/components";
+import ServerSideRender from "@wordpress/server-side-render";
 
-import _toNumber from '../../src/js/blocks/function/_toNumber';
-import * as BlockOption from './inspector-controls';
-import PanelThumbnail from './inspector-controls/thumbnail';
-import PanelSearchOption from './inspector-controls/search-option';
-import PanelAdvancedSearch from './inspector-controls/advanced-search';
-import PanelAdvancedDesign from './inspector-controls/advanced-design';
-import PanelBasicOption from './inspector-controls/basic-option';
+import _toNumber from "../../src/js/blocks/function/_toNumber";
+import * as BlockOption from "./inspector-controls";
+import PanelThumbnail from "./inspector-controls/thumbnail";
+import PanelSearchOption from "./inspector-controls/search-option";
+import PanelAdvancedSearch from "./inspector-controls/advanced-search";
+import PanelAdvancedDesign from "./inspector-controls/advanced-design";
+import PanelBasicOption from "./inspector-controls/basic-option";
 
-const Posts = ( props ) => {
-	const { attributes, setAttributes } = props;
-	const {
-		colMobile,
-		colTablet,
-		colPc,
-		showDate,
-		showCategory,
-		showExcerpt,
-		listType,
-	} = attributes;
-	return (
-		<div className={ 'ystdtb-posts' }>
-			<>
-				<InspectorControls>
-					<PanelBasicOption { ...props } />
-					<PanelBody title={ __( '表示設定', 'ystandard-toolbox' ) }>
-						<BlockOption.ListDesign { ...props } />
-						<div className="ystdtb-block-label">
-							{ __( '表示列数', 'ystandard-toolbox' ) }
-						</div>
-						<RangeControl
-							label={ __( 'デスクトップ', 'ystandard-toolbox' ) }
-							beforeIcon={ 'desktop' }
-							value={ colPc }
-							onChange={ ( value ) => {
-								setAttributes( {
-									colPc: _toNumber( value, 1, 6, 3 ),
-								} );
-							} }
-							min={ 1 }
-							max={ 6 }
-						/>
-						<RangeControl
-							label={ __( 'タブレット', 'ystandard-toolbox' ) }
-							beforeIcon={ 'tablet' }
-							value={ colTablet }
-							onChange={ ( value ) => {
-								setAttributes( {
-									colTablet: _toNumber( value, 1, 6, 3 ),
-								} );
-							} }
-							min={ 1 }
-							max={ 6 }
-						/>
-						<RangeControl
-							label={ __( 'モバイル', 'ystandard-toolbox' ) }
-							beforeIcon={ 'smartphone' }
-							value={ colMobile }
-							onChange={ ( value ) => {
-								setAttributes( {
-									colMobile: _toNumber( value, 1, 6, 1 ),
-								} );
-							} }
-							min={ 1 }
-							max={ 6 }
-						/>
-					</PanelBody>
-					<PanelThumbnail { ...props } />
-					<PanelBody
-						initialOpen={ false }
-						title={ __(
-							'日付・カテゴリー・概要の表示',
-							'ystandard-toolbox'
-						) }
-					>
-						<ToggleControl
-							label={ __(
-								'日付を表示する',
-								'ystandard-toolbox'
-							) }
-							onChange={ () => {
-								setAttributes( {
-									showDate: ! showDate,
-								} );
-							} }
-							checked={ showDate }
-						/>
-						<ToggleControl
-							label={ __(
-								'カテゴリーを表示する',
-								'ystandard-toolbox'
-							) }
-							onChange={ () => {
-								setAttributes( {
-									showCategory: ! showCategory,
-								} );
-							} }
-							checked={ showCategory }
-						/>
-						{ 'simple' !== listType && (
-							<>
-								<ToggleControl
-									label={ __(
-										'概要を表示する',
-										'ystandard-toolbox'
-									) }
-									onChange={ () => {
-										setAttributes( {
-											showExcerpt: ! showExcerpt,
-										} );
-									} }
-									checked={ showExcerpt }
-								/>
-								<BlockOption.ExcerptLength { ...props } />
-							</>
-						) }
-					</PanelBody>
-					<PanelSearchOption { ...props } />
-					<PanelAdvancedSearch { ...props } />
-					<PanelAdvancedDesign { ...props } />
-				</InspectorControls>
-			</>
-			<Disabled>
-				<ServerSideRender
-					block="ystdtb/posts"
-					attributes={ attributes }
-				/>
-			</Disabled>
-		</div>
-	);
+const Posts = (props) => {
+  const { attributes, setAttributes } = props;
+  const {
+    colMobile,
+    colTablet,
+    colPc,
+    showDate,
+    showCategory,
+    showExcerpt,
+    listType,
+  } = attributes;
+  return (
+    <div className={"ystdtb-posts"}>
+      <>
+        <InspectorControls>
+          <PanelBasicOption {...props} />
+          <PanelBody title={__("表示設定", "ystandard-toolbox")}>
+            <BlockOption.ListDesign {...props} />
+            <div className="ystdtb-block-label">
+              {__("表示列数", "ystandard-toolbox")}
+            </div>
+            <RangeControl
+              label={__("デスクトップ", "ystandard-toolbox")}
+              beforeIcon={"desktop"}
+              value={colPc}
+              onChange={(value) => {
+                setAttributes({
+                  colPc: _toNumber(value, 1, 6, 3),
+                });
+              }}
+              min={1}
+              max={6}
+            />
+            <RangeControl
+              label={__("タブレット", "ystandard-toolbox")}
+              beforeIcon={"tablet"}
+              value={colTablet}
+              onChange={(value) => {
+                setAttributes({
+                  colTablet: _toNumber(value, 1, 6, 3),
+                });
+              }}
+              min={1}
+              max={6}
+            />
+            <RangeControl
+              label={__("モバイル", "ystandard-toolbox")}
+              beforeIcon={"smartphone"}
+              value={colMobile}
+              onChange={(value) => {
+                setAttributes({
+                  colMobile: _toNumber(value, 1, 6, 1),
+                });
+              }}
+              min={1}
+              max={6}
+            />
+          </PanelBody>
+          <PanelThumbnail {...props} />
+          <PanelBody
+            initialOpen={false}
+            title={__("日付・カテゴリー・概要の表示", "ystandard-toolbox")}
+          >
+            <ToggleControl
+              label={__("日付を表示する", "ystandard-toolbox")}
+              onChange={() => {
+                setAttributes({
+                  showDate: !showDate,
+                });
+              }}
+              checked={showDate}
+            />
+            <ToggleControl
+              label={__("カテゴリーを表示する", "ystandard-toolbox")}
+              onChange={() => {
+                setAttributes({
+                  showCategory: !showCategory,
+                });
+              }}
+              checked={showCategory}
+            />
+            {"simple" !== listType && (
+              <>
+                <ToggleControl
+                  label={__("概要を表示する", "ystandard-toolbox")}
+                  onChange={() => {
+                    setAttributes({
+                      showExcerpt: !showExcerpt,
+                    });
+                  }}
+                  checked={showExcerpt}
+                />
+                <BlockOption.ExcerptLength {...props} />
+              </>
+            )}
+          </PanelBody>
+          <PanelSearchOption {...props} />
+          <PanelAdvancedSearch {...props} />
+          <PanelAdvancedDesign {...props} />
+        </InspectorControls>
+      </>
+      <Disabled>
+        <ServerSideRender block="ystdtb/posts" attributes={attributes} />
+      </Disabled>
+    </div>
+  );
 };
 
 export default Posts;
