31a69dcf8df7034cb1cdbd9981dec8524fc88d9c
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-03-28 09:41:46 +0900

    lint

--- blocks/posts/inspector-controls/search-option/post-type.js
+++ blocks/posts/inspector-controls/search-option/post-type.js
@@ -1,30 +1,30 @@
 /**
  * WordPress.
  */
-import { __ } from "@wordpress/i18n";
-import { BaseControl } from "@wordpress/components";
-import PostTypeSelect from "@ystd/components/post-type-select";
+import { __ } from '@wordpress/i18n';
+import { BaseControl } from '@wordpress/components';
+import PostTypeSelect from '@ystd/components/post-type-select';
 
-const PostType = ({ attributes, setAttributes }) => {
-  const { postType } = attributes;
-  const handleOnChange = (newValue) => {
-    setAttributes({
-      postType: newValue,
-      taxonomy: undefined,
-      termSlug: undefined,
-      postIn: undefined,
-      postNameIn: undefined,
-      postParent: undefined,
-    });
-  };
-  return (
-    <BaseControl>
-      <PostTypeSelect
-        label={__("投稿タイプ", "ystandard-toolbox")}
-        value={postType}
-        onChange={handleOnChange}
-      />
-    </BaseControl>
-  );
+const PostType = ( { attributes, setAttributes } ) => {
+	const { postType } = attributes;
+	const handleOnChange = ( newValue ) => {
+		setAttributes( {
+			postType: newValue,
+			taxonomy: undefined,
+			termSlug: undefined,
+			postIn: undefined,
+			postNameIn: undefined,
+			postParent: undefined,
+		} );
+	};
+	return (
+		<BaseControl>
+			<PostTypeSelect
+				label={ __( '投稿タイプ', 'ystandard-toolbox' ) }
+				value={ postType }
+				onChange={ handleOnChange }
+			/>
+		</BaseControl>
+	);
 };
 export default PostType;
--- blocks/posts/inspector-controls/search-option/taxonomy.js
+++ blocks/posts/inspector-controls/search-option/taxonomy.js
@@ -1,30 +1,30 @@
 /**
  * WordPress.
  */
-import { __ } from "@wordpress/i18n";
-import { BaseControl } from "@wordpress/components";
-import TaxonomySelect from "@ystd/components/taxonomy-select";
+import { __ } from '@wordpress/i18n';
+import { BaseControl } from '@wordpress/components';
+import TaxonomySelect from '@ystd/components/taxonomy-select';
 
-const Taxonomy = ({ attributes, setAttributes }) => {
-  const { taxonomy, postType } = attributes;
-  const handleOnChange = (newValue) => {
-    setAttributes({
-      taxonomy: newValue,
-      termSlug: undefined,
-      postIn: undefined,
-      postNameIn: undefined,
-      postParent: undefined,
-    });
-  };
-  return (
-    <BaseControl>
-      <TaxonomySelect
-        label={__("分類", "ystandard-toolbox")}
-        value={taxonomy}
-        postType={postType}
-        onChange={handleOnChange}
-      />
-    </BaseControl>
-  );
+const Taxonomy = ( { attributes, setAttributes } ) => {
+	const { taxonomy, postType } = attributes;
+	const handleOnChange = ( newValue ) => {
+		setAttributes( {
+			taxonomy: newValue,
+			termSlug: undefined,
+			postIn: undefined,
+			postNameIn: undefined,
+			postParent: undefined,
+		} );
+	};
+	return (
+		<BaseControl>
+			<TaxonomySelect
+				label={ __( '分類', 'ystandard-toolbox' ) }
+				value={ taxonomy }
+				postType={ postType }
+				onChange={ handleOnChange }
+			/>
+		</BaseControl>
+	);
 };
 export default Taxonomy;
