98e9cc7ae0099763b8b16ff78fccd0719a211805
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2023-03-30 17:59:14 +0900

    FAQブロックマージ調整

--- blocks/faq/item/deprecated/deprecatedOld.js
+++ blocks/faq/item/deprecated/deprecatedOld.js
@@ -1,234 +1,258 @@
-import classnames from "classnames";
-import SVGIcon from "../../../../src/js/component/svg-icon";
+import classnames from 'classnames';
+import SVGIcon from '@aktk/components/svg-icon';
 import {
-  InnerBlocks,
-  getColorClassName,
-  getFontSizeClass,
-} from "@wordpress/block-editor";
+	InnerBlocks,
+	getColorClassName,
+	getFontSizeClass,
+} from '@wordpress/block-editor';
 
 const blockAttributes = {
-  faqType: {
-    type: "string",
-    default: "q",
-  },
-  faqTextColor: {
-    type: "string",
-  },
-  customFaqTextColor: {
-    type: "string",
-  },
-  faqBackgroundColor: {
-    type: "string",
-  },
-  customFaqBackgroundColor: {
-    type: "string",
-  },
-  faqBorderType: {
-    type: "string",
-    default: "",
-  },
-  faqBorderSize: {
-    type: "number",
-    default: 0,
-  },
-  faqBorderColor: {
-    type: "string",
-  },
-  customFaqBorderColor: {
-    type: "string",
-  },
-  labelPosition: {
-    type: "string",
-    default: "center",
-  },
-  labelSize: {
-    type: "string",
-  },
-  customLabelSize: {
-    type: "number",
-  },
-  labelColor: {
-    type: "string",
-  },
-  customLabelColor: {
-    type: "string",
-  },
-  labelBold: {
-    type: "bool",
-    default: true,
-  },
-  labelBackgroundColor: {
-    type: "string",
-  },
-  customLabelBackgroundColor: {
-    type: "string",
-  },
-  labelBorderSize: {
-    type: "number",
-    default: 0,
-  },
-  labelBorderRadius: {
-    type: "number",
-    default: 0,
-  },
-  labelBorderColor: {
-    type: "string",
-  },
-  customLabelBorderColor: {
-    type: "string",
-  },
-  accordionArrowColor: {
-    type: "string",
-  },
-  customAccordionArrowColor: {
-    type: "string",
-  },
+	faqType: {
+		type: 'string',
+		default: 'q',
+	},
+	faqTextColor: {
+		type: 'string',
+	},
+	customFaqTextColor: {
+		type: 'string',
+	},
+	faqBackgroundColor: {
+		type: 'string',
+	},
+	customFaqBackgroundColor: {
+		type: 'string',
+	},
+	faqBorderType: {
+		type: 'string',
+		default: '',
+	},
+	faqBorderSize: {
+		type: 'number',
+		default: 0,
+	},
+	faqBorderColor: {
+		type: 'string',
+	},
+	customFaqBorderColor: {
+		type: 'string',
+	},
+	labelPosition: {
+		type: 'string',
+		default: 'center',
+	},
+	labelSize: {
+		type: 'string',
+	},
+	customLabelSize: {
+		type: 'number',
+	},
+	labelColor: {
+		type: 'string',
+	},
+	customLabelColor: {
+		type: 'string',
+	},
+	labelBold: {
+		type: 'bool',
+		default: true,
+	},
+	labelBackgroundColor: {
+		type: 'string',
+	},
+	customLabelBackgroundColor: {
+		type: 'string',
+	},
+	labelBorderSize: {
+		type: 'number',
+		default: 0,
+	},
+	labelBorderRadius: {
+		type: 'number',
+		default: 0,
+	},
+	labelBorderColor: {
+		type: 'string',
+	},
+	customLabelBorderColor: {
+		type: 'string',
+	},
+	accordionArrowColor: {
+		type: 'string',
+	},
+	customAccordionArrowColor: {
+		type: 'string',
+	},
 };
 
 export const deprecatedOld = [
-  {
-    attributes: {
-      ...blockAttributes,
-    },
-    supports: {
-      align: false,
-      className: false,
-      lightBlockWrapper: true,
-      reusable: false,
-    },
-    save({ attributes }) {
-      const {
-        className,
-        faqType,
-        faqBorderType,
-        faqBorderSize,
-        labelPosition,
-        labelBorderSize,
-        labelBorderRadius,
-        faqTextColor,
-        customFaqTextColor,
-        faqBackgroundColor,
-        customFaqBackgroundColor,
-        faqBorderColor,
-        customFaqBorderColor,
-        labelSize,
-        customLabelSize,
-        labelBold,
-        labelColor,
-        customLabelColor,
-        labelBackgroundColor,
-        customLabelBackgroundColor,
-        labelBorderColor,
-        customLabelBorderColor,
-        accordionArrowColor,
-        customAccordionArrowColor,
-      } = attributes;
+	{
+		attributes: {
+			...blockAttributes,
+		},
+		supports: {
+			align: false,
+			className: false,
+			lightBlockWrapper: true,
+			reusable: false,
+		},
+		save( { attributes } ) {
+			const {
+				className,
+				faqType,
+				faqBorderType,
+				faqBorderSize,
+				labelPosition,
+				labelBorderSize,
+				labelBorderRadius,
+				faqTextColor,
+				customFaqTextColor,
+				faqBackgroundColor,
+				customFaqBackgroundColor,
+				faqBorderColor,
+				customFaqBorderColor,
+				labelSize,
+				customLabelSize,
+				labelBold,
+				labelColor,
+				customLabelColor,
+				labelBackgroundColor,
+				customLabelBackgroundColor,
+				labelBorderColor,
+				customLabelBorderColor,
+				accordionArrowColor,
+				customAccordionArrowColor,
+			} = attributes;
 
-      const faqTextColorClass = getColorClassName("color", faqTextColor);
-      const faqBackgroundColorClass = getColorClassName(
-        "background-color",
-        faqBackgroundColor
-      );
-      const faqBorderColorClass = getColorClassName(
-        "border-color",
-        faqBorderColor
-      );
-      const labelColorClass = getColorClassName("color", labelColor);
-      const labelBackgroundColorClass = getColorClassName(
-        "background-color",
-        labelBackgroundColor
-      );
-      const labelBorderColorClass = getColorClassName(
-        "border-color",
-        labelBorderColor
-      );
+			const faqTextColorClass = getColorClassName(
+				'color',
+				faqTextColor
+			);
+			const faqBackgroundColorClass = getColorClassName(
+				'background-color',
+				faqBackgroundColor
+			);
+			const faqBorderColorClass = getColorClassName(
+				'border-color',
+				faqBorderColor
+			);
+			const labelColorClass = getColorClassName( 'color', labelColor );
+			const labelBackgroundColorClass = getColorClassName(
+				'background-color',
+				labelBackgroundColor
+			);
+			const labelBorderColorClass = getColorClassName(
+				'border-color',
+				labelBorderColor
+			);
 
-      const labelSizeClass = getFontSizeClass(labelSize);
+			const labelSizeClass = getFontSizeClass( labelSize );
 
-      const itemClasses = classnames("ystdtb-faq-item", className, {
-        [`is-faq--${faqType}`]: faqType,
-        [`has-border-${faqBorderType}`]: "" !== faqBorderType,
-        "has-background": faqBackgroundColor || customFaqBackgroundColor,
-        [faqBackgroundColorClass]: faqBackgroundColorClass,
-        "has-border": faqBorderColor || customFaqBorderColor,
-        [faqBorderColorClass]: faqBorderColorClass,
-      });
-      const itemStyles = {
-        backgroundColor: faqBackgroundColorClass
-          ? undefined
-          : customFaqBackgroundColor,
-        borderColor: faqBorderColorClass ? undefined : customFaqBorderColor,
-        borderWidth: 0 === faqBorderSize ? undefined : faqBorderSize + "px",
-      };
+			const itemClasses = classnames( 'ystdtb-faq-item', className, {
+				[ `is-faq--${ faqType }` ]: faqType,
+				[ `has-border-${ faqBorderType }` ]: '' !== faqBorderType,
+				'has-background':
+					faqBackgroundColor || customFaqBackgroundColor,
+				[ faqBackgroundColorClass ]: faqBackgroundColorClass,
+				'has-border': faqBorderColor || customFaqBorderColor,
+				[ faqBorderColorClass ]: faqBorderColorClass,
+			} );
+			const itemStyles = {
+				backgroundColor: faqBackgroundColorClass
+					? undefined
+					: customFaqBackgroundColor,
+				borderColor: faqBorderColorClass
+					? undefined
+					: customFaqBorderColor,
+				borderWidth:
+					0 === faqBorderSize ? undefined : faqBorderSize + 'px',
+			};
 
-      const labelClasses = classnames("ystdtb-faq-item__label", {
-        [labelSizeClass]: labelSizeClass,
-        "has-text-color": labelColor || customLabelColor,
-        [labelColorClass]: labelColorClass,
-        "has-background": labelBackgroundColor || customLabelBackgroundColor,
-        [labelBackgroundColorClass]: labelBackgroundColorClass,
-        "has-border": labelBorderColor || customLabelBorderColor,
-        [labelBorderColorClass]: labelBorderColorClass,
-        "has-padding":
-          labelBackgroundColor ||
-          customLabelBackgroundColor ||
-          labelBorderColor ||
-          customLabelBorderColor ||
-          labelBorderSize,
-      });
-      const labelStyles = {
-        fontSize: labelSizeClass ? undefined : customLabelSize,
-        color: labelColorClass ? undefined : customLabelColor,
-        fontWeight: labelBold ? undefined : 400,
-        backgroundColor: labelBackgroundColorClass
-          ? undefined
-          : customLabelBackgroundColor,
-        borderColor: labelBorderColorClass ? undefined : customLabelBorderColor,
-        borderWidth: 0 === labelBorderSize ? undefined : labelBorderSize + "px",
-        borderRadius:
-          0 === labelBorderRadius ? undefined : labelBorderRadius + "px",
-        alignSelf: "center" === labelPosition ? undefined : labelPosition,
-      };
+			const labelClasses = classnames( 'ystdtb-faq-item__label', {
+				[ labelSizeClass ]: labelSizeClass,
+				'has-text-color': labelColor || customLabelColor,
+				[ labelColorClass ]: labelColorClass,
+				'has-background':
+					labelBackgroundColor || customLabelBackgroundColor,
+				[ labelBackgroundColorClass ]: labelBackgroundColorClass,
+				'has-border': labelBorderColor || customLabelBorderColor,
+				[ labelBorderColorClass ]: labelBorderColorClass,
+				'has-padding':
+					labelBackgroundColor ||
+					customLabelBackgroundColor ||
+					labelBorderColor ||
+					customLabelBorderColor ||
+					labelBorderSize,
+			} );
+			const labelStyles = {
+				fontSize: labelSizeClass ? undefined : customLabelSize,
+				color: labelColorClass ? undefined : customLabelColor,
+				fontWeight: labelBold ? undefined : 400,
+				backgroundColor: labelBackgroundColorClass
+					? undefined
+					: customLabelBackgroundColor,
+				borderColor: labelBorderColorClass
+					? undefined
+					: customLabelBorderColor,
+				borderWidth:
+					0 === labelBorderSize ? undefined : labelBorderSize + 'px',
+				borderRadius:
+					0 === labelBorderRadius
+						? undefined
+						: labelBorderRadius + 'px',
+				alignSelf:
+					'center' === labelPosition ? undefined : labelPosition,
+			};
 
-      const labelTextClasses = classnames("ystdtb-faq-item__label-text", {});
+			const labelTextClasses = classnames(
+				'ystdtb-faq-item__label-text',
+				{}
+			);
 
-      const contentsClasses = classnames("ystdtb-faq-item__contents", {
-        "has-text-color": faqTextColor || customFaqTextColor,
-        [faqTextColorClass]: faqTextColorClass,
-      });
+			const contentsClasses = classnames( 'ystdtb-faq-item__contents', {
+				'has-text-color': faqTextColor || customFaqTextColor,
+				[ faqTextColorClass ]: faqTextColorClass,
+			} );
 
-      const contentsStyles = {
-        color: faqTextColorClass ? undefined : customFaqTextColor,
-      };
+			const contentsStyles = {
+				color: faqTextColorClass ? undefined : customFaqTextColor,
+			};
 
-      const accordionArrowColorClass = getColorClassName(
-        "color",
-        accordionArrowColor
-      );
-      const accordionArrowClass = classnames("ystdtb-faq-item__arrow", {
-        "has-text-color": accordionArrowColorClass || customAccordionArrowColor,
-        [accordionArrowColorClass]: accordionArrowColorClass,
-      });
-      const accordionArrowStyle = {
-        color: accordionArrowColorClass ? undefined : customAccordionArrowColor,
-        alignSelf: "center" === labelPosition ? undefined : labelPosition,
-      };
+			const accordionArrowColorClass = getColorClassName(
+				'color',
+				accordionArrowColor
+			);
+			const accordionArrowClass = classnames( 'ystdtb-faq-item__arrow', {
+				'has-text-color':
+					accordionArrowColorClass || customAccordionArrowColor,
+				[ accordionArrowColorClass ]: accordionArrowColorClass,
+			} );
+			const accordionArrowStyle = {
+				color: accordionArrowColorClass
+					? undefined
+					: customAccordionArrowColor,
+				alignSelf:
+					'center' === labelPosition ? undefined : labelPosition,
+			};
 
-      return (
-        <div className={itemClasses} style={itemStyles}>
-          <div className={labelClasses} style={labelStyles}>
-            <span className={labelTextClasses}>{faqType}</span>
-          </div>
-          <div className={contentsClasses} style={contentsStyles}>
-            <InnerBlocks.Content />
-          </div>
-          {"q" === faqType && (
-            <div className={accordionArrowClass} style={accordionArrowStyle}>
-              <SVGIcon name={"chevron-down"} />
-            </div>
-          )}
-        </div>
-      );
-    },
-  },
+			return (
+				<div className={ itemClasses } style={ itemStyles }>
+					<div className={ labelClasses } style={ labelStyles }>
+						<span className={ labelTextClasses }>{ faqType }</span>
+					</div>
+					<div className={ contentsClasses } style={ contentsStyles }>
+						<InnerBlocks.Content />
+					</div>
+					{ 'q' === faqType && (
+						<div
+							className={ accordionArrowClass }
+							style={ accordionArrowStyle }
+						>
+							<SVGIcon name={ 'chevron-down' } />
+						</div>
+					) }
+				</div>
+			);
+		},
+	},
 ];
--- blocks/faq/item/index.js
+++ blocks/faq/item/index.js
@@ -1,28 +1,28 @@
-import { ystdtbConfig } from "@ystd/config";
-import edit from "./edit";
-import save from "./save";
-import { MessageCircle } from "react-feather";
-import { attributes, supports } from "./config";
-import { registerBlockType } from "@wordpress/blocks";
-import { deprecated } from "./deprecated";
-import { __ } from "@wordpress/i18n";
+import { ystdtbConfig } from '@aktk/config';
+import edit from './edit';
+import save from './save';
+import { MessageCircle } from 'react-feather';
+import { attributes, supports } from './config';
+import { registerBlockType } from '@wordpress/blocks';
+import { deprecated } from './deprecated';
+import { __ } from '@wordpress/i18n';
 
-registerBlockType("ystdtb/faq-item", {
-  apiVersion: 2,
-  title: __("Q&A項目", "ystandard-toolbox"),
-  description: __("Q&Aの内容", "ystandard-toolbox"),
-  icon: (
-    <MessageCircle
-      stroke={ystdtbConfig.color.iconForegroundChild}
-      style={{ fill: "none" }}
-    />
-  ),
-  keywords: [__("faq"), __("Q&A"), "faq"],
-  category: ystdtbConfig.category.common,
-  supports,
-  attributes,
-  parent: ["ystdtb/faq"],
-  edit,
-  save,
-  deprecated,
-});
+registerBlockType( 'ystdtb/faq-item', {
+	apiVersion: 2,
+	title: __( 'Q&A項目', 'ystandard-toolbox' ),
+	description: __( 'Q&Aの内容', 'ystandard-toolbox' ),
+	icon: (
+		<MessageCircle
+			stroke={ ystdtbConfig.color.iconForegroundChild }
+			style={ { fill: 'none' } }
+		/>
+	),
+	keywords: [ __( 'faq' ), __( 'Q&A' ), 'faq' ],
+	category: ystdtbConfig.category.common,
+	supports,
+	attributes,
+	parent: [ 'ystdtb/faq' ],
+	edit,
+	save,
+	deprecated,
+} );
