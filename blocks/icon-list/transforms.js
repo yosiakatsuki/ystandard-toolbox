a2ff39fc3270a1a5be184995f62bb0b2aec8ef02
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2024-01-05 00:31:09 +0900

    fix #96

--- blocks/icon-list/transforms.js
+++ blocks/icon-list/transforms.js
@@ -1,48 +1,61 @@
-import { createBlock } from '@wordpress/blocks';
+import {createBlock} from '@wordpress/blocks';
 
 const transforms = {
-	from: [
-		{
-			type: 'block',
-			blocks: [ 'core/list' ],
-			transform: ( attributes, innerBlocks ) => {
-				return createBlock(
-					'ystdtb/icon-list',
-					{
-						values: attributes.values,
-						backgroundColor: attributes.backgroundColor,
-						customBackgroundColor: attributes.customBackgroundColor,
-						textColor: attributes.textColor,
-						customTextColor: attributes.customTextColor,
-						fontSize: attributes.fontSize,
-						customFontSize: attributes.customFontSize,
-					},
-					innerBlocks
-				);
-			},
-		},
-	],
-	to: [
-		{
-			type: 'block',
-			blocks: [ 'core/list' ],
-			transform: ( attributes, innerBlocks ) => {
-				return createBlock(
-					'core/list',
-					{
-						values: attributes.values,
-						backgroundColor: attributes.backgroundColor,
-						customBackgroundColor: attributes.customBackgroundColor,
-						textColor: attributes.textColor,
-						customTextColor: attributes.customTextColor,
-						fontSize: attributes.fontSize,
-						customFontSize: attributes.customFontSize,
-					},
-					innerBlocks
-				);
-			},
-		},
-	],
+    from: [
+        {
+            type: 'block',
+            blocks: ['core/list'],
+            transform: (attributes, innerBlocks) => {
+                const newInnerBlocks = innerBlocks.map((block) => {
+                    return createBlock(
+                        'ystdtb/icon-list-item',
+                        block.attributes,
+                    );
+                });
+
+                return createBlock(
+                    'ystdtb/icon-list',
+                    {
+                        values: attributes.values,
+                        backgroundColor: attributes.backgroundColor,
+                        customBackgroundColor: attributes.customBackgroundColor,
+                        textColor: attributes.textColor,
+                        customTextColor: attributes.customTextColor,
+                        fontSize: attributes.fontSize,
+                        customFontSize: attributes.customFontSize,
+                    },
+                    newInnerBlocks
+                );
+            },
+        },
+    ],
+    to: [
+        {
+            type: 'block',
+            blocks: ['core/list'],
+            transform: (attributes, innerBlocks) => {
+                const newInnerBlocks = innerBlocks.map((block) => {
+                    return createBlock(
+                        'core/list-item',
+                        block.attributes,
+                    );
+                });
+                return createBlock(
+                    'core/list',
+                    {
+                        values: attributes.values,
+                        backgroundColor: attributes.backgroundColor,
+                        customBackgroundColor: attributes.customBackgroundColor,
+                        textColor: attributes.textColor,
+                        customTextColor: attributes.customTextColor,
+                        fontSize: attributes.fontSize,
+                        customFontSize: attributes.customFontSize,
+                    },
+                    newInnerBlocks
+                );
+            },
+        },
+    ],
 };
 
 export default transforms;
