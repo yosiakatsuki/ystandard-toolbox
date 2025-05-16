45683a344774d07436d71296a5959d1668876612
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-05-16 12:05:37 +0900

    custom spacing select 更新

--- src/aktk-block-components/components/custom-spacing-select/types.ts
+++ src/aktk-block-components/components/custom-spacing-select/types.ts
@@ -5,8 +5,18 @@ export interface Spacing {
 	left?: string;
 }
 
-export interface CustomSpacing {
+export interface ResponsiveSpacing {
 	desktop?: Spacing;
 	tablet?: Spacing;
 	mobile?: Spacing;
+}
+
+export interface ResponsiveSpacingSelectProps {
+	value: ResponsiveSpacing | undefined;
+	onChange: ( newValue?: ResponsiveSpacing ) => void;
+	responsiveControlStyle?: 'vertical' | 'horizontal';
+	sides?: ( 'top' | 'right' | 'bottom' | 'left' )[];
+	minimumCustomValue?: number;
+	useResponsive?: boolean;
+	showResetButton?: boolean;
 }
