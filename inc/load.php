d83e6a8d2fb4b4a964c0b19327ffb16af829f557
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-05-20 11:33:46 +0900

    旧設定画面削除

--- inc/load.php
+++ inc/load.php
@@ -61,8 +61,6 @@ require_once __DIR__ . '/polyfill/class-polyfill.php';
 
 // Admin.
 require_once __DIR__ . '/admin/class-admin-utility.php';
-require_once __DIR__ . '/menu-page/class-menu-page.php';
-require_once __DIR__ . '/admin/class-plugin-menu.php';
 if ( file_exists( __DIR__ . '/update/class-update.php' ) ) {
 	require_once __DIR__ . '/update/class-update.php';
 }
