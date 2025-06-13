4eb1315af691b4c96b21b8dd2eeb5dd6daea9cb3
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-06-14 01:36:52 +0900

    Delete boolean.js

--- src/js/helper/boolean.js
+++
@@ -1,6 +0,0 @@
-/**
- * @deprecated '@aktk/block-components/utils/boolean'のtoBoolを使用してください
- */
-export const toBool = ( value ) => {
-	return true === value || 'true' === value || 1 === value || '1' === value;
-};
