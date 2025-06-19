9d6215d1f91031cf979fed1b74a2b7a847f9a2a4
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-06-19 12:48:43 +0900

    コード追加エディタータブコンポーネントをTypeScript化して国際化対応

--- src/plugin-settings/add-code/editor-tab.tsx
+++ src/plugin-settings/add-code/editor-tab.tsx
@@ -1,29 +1,48 @@
 /**
- * WordPress
+ * WordPress Dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Plugin Dependencies
  */
 import EditorPanel from './editor-panel';
-import SettingsTab from '@aktk/plugin-settings/components/settings-tab';
+import { SettingsTab } from '@aktk/plugin-settings/components/settings-tab';
 
-const EditorTab = () => {
+/**
+ * コード追加タブの定義
+ */
+interface CodeTab {
+	name: string;
+	title: string;
+}
+
+const TABS: CodeTab[] = [
+	{
+		name: 'head',
+		title: __( '<head>内に追加するコード', 'ystandard-toolbox' ),
+	},
+	{
+		name: 'body_open',
+		title: __( '<body>直後に追加するコード', 'ystandard-toolbox' ),
+	},
+	{
+		name: 'body_close',
+		title: __( '</body>直前に追加するコード', 'ystandard-toolbox' ),
+	},
+];
+
+/**
+ * コード追加エディタータブコンポーネント
+ * ヘッダー、ボディ開始、ボディ終了部分のコード編集タブを提供
+ */
+export default function EditorTab(): JSX.Element {
 	return (
-		<SettingsTab
-			tabs={ [
-				{
-					name: 'head',
-					title: '<head>内に追加するコード',
-				},
-				{
-					name: 'body_open',
-					title: '<body>直後に追加するコード',
-				},
-				{
-					name: 'body_close',
-					title: '</body>直前に追加するコード',
-				},
-			] }
-		>
-			{ ( tab ) => <EditorPanel tab={ tab } /> }
+		<SettingsTab tabs={ TABS }>
+			{ ( tab ) => {
+				const codeTab = tab as CodeTab;
+				return <EditorPanel tab={ codeTab } />;
+			} }
 		</SettingsTab>
 	);
-};
-export default EditorTab;
+}
