cdba52cf7c5f29fa1afdd899f731f6feaba2beee
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-08-05 11:44:10 +0900

    バナーリンク：HTMLタグ選択の調整

--- src/blocks/block-library/banner-link/config.ts
+++ src/blocks/block-library/banner-link/config.ts
@@ -125,14 +125,14 @@ export const supports = {
 };
 
 export const textHtmlTag = [
-	{ value: 'div', label: 'div' },
-	{ value: 'h1', label: 'h1' },
-	{ value: 'h2', label: 'h2' },
-	{ value: 'h3', label: 'h3' },
-	{ value: 'h4', label: 'h4' },
-	{ value: 'h5', label: 'h5' },
-	{ value: 'h6', label: 'h6' },
-	{ value: 'p', label: 'p' },
+	{ key: 'div', name: 'div' },
+	{ key: 'h1', name: 'h1' },
+	{ key: 'h2', name: 'h2' },
+	{ key: 'h3', name: 'h3' },
+	{ key: 'h4', name: 'h4' },
+	{ key: 'h5', name: 'h5' },
+	{ key: 'h6', name: 'h6' },
+	{ key: 'p', name: 'p' },
 ];
 
 export const headingTag = [ 'h1', 'h2', 'h3', 'h4', 'h5', 'h6' ];
