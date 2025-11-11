b5ce05a306a25baef6a18a3f95110d8e5c924d33
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-11-11 09:03:03 +0900

    アイコンリストブロック移行開始

---
+++ src/blocks/block-library/icon-list/index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * アイコンリスト（外側）ブロック
+ *
+ * @package ystandard-toolbox
+ */
+
+namespace ystandard_toolbox;
+
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Class Icon_List_Block.
+ */
+class Icon_List_Block {
+	/**
+	 * Instance.
+	 *
+	 * @var Icon_List_Block
+	 */
+	private static $instance;
+
+	/**
+	 * Constructor.
+	 */
+	private function __construct() {
+		add_action( 'init', [ $this, 'register_block' ], 100 );
+	}
+
+	/**
+	 * Instance.
+	 *
+	 * @return Icon_List_Block
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * ブロック登録
+	 *
+	 * @return void
+	 */
+	public function register_block() {
+		register_block_type( __DIR__ );
+	}
+}
+
+Icon_List_Block::get_instance();
