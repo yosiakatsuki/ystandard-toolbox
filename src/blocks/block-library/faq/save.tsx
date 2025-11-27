dde682e478c6cbe8e0ab4b56507bdad47106abc1
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-11-27 22:48:19 +0900

    FAQ：saveのclass,styleをutils処理に変更

--- src/blocks/block-library/faq/save.tsx
+++ src/blocks/block-library/faq/save.tsx
@@ -1,18 +1,16 @@
-import classnames from 'classnames';
 /*
  * WordPress Dependencies
  */
-import {
-	InnerBlocks,
-	useBlockProps,
-	// @ts-ignore
-	getColorClassName,
-} from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 /*
  * Plugin Dependencies
  */
 import type { FaqBlockAttributes } from './types';
+import {
+	getFaqClassNames,
+	getFaqStyle,
+} from '@aktk/blocks/block-library/faq/utils';
 
 type FaqSaveProps = {
 	attributes: FaqBlockAttributes;
@@ -20,54 +18,13 @@ type FaqSaveProps = {
 
 /**
  * FAQブロック保存コンポーネント
+ * @param root0
+ * @param root0.attributes
  */
-export default function FaqSave( { attributes }: FaqSaveProps ) {
-	const {
-		isAccordion,
-		borderType,
-		borderSize,
-		backgroundColor,
-		customBackgroundColor,
-		borderColor,
-		customBorderColor,
-	} = attributes;
-
-	// 背景色クラス名を取得
-	const backgroundColorClass = getColorClassName(
-		'background-color',
-		backgroundColor
-	);
-
-	// 枠線色クラス名を取得
-	const borderColorClass = getColorClassName( 'border-color', borderColor );
-
-	// クラス名
-	const faqClasses = classnames( 'ystdtb-faq', {
-		'is-accordion': isAccordion,
-		'has-padding': 'all' === borderType || backgroundColor,
-		'has-background': backgroundColor || customBackgroundColor,
-		[ backgroundColorClass ]: backgroundColorClass,
-		'has-border': borderSize || borderColor || customBorderColor,
-		[ borderColorClass ]: borderColorClass,
-		[ `border-type--${ borderType }` ]: '' !== borderType,
-	} );
-
-	// インラインスタイル
-	const faqStyle = {
-		backgroundColor: backgroundColorClass
-			? undefined
-			: customBackgroundColor,
-		borderColor: borderColorClass ? undefined : customBorderColor,
-		borderWidth: 'all' === borderType ? borderSize : undefined,
-		borderBottomWidth:
-			'bottom' === borderType || 'all' === borderType
-				? borderSize
-				: undefined,
-	};
-
+export default function FaqSave( { attributes }: FaqSaveProps ): JSX.Element {
 	const blockProps = useBlockProps.save( {
-		className: faqClasses,
-		style: faqStyle,
+		className: getFaqClassNames( attributes ),
+		style: getFaqStyle( attributes ),
 	} );
 
 	return (
--- src/blocks/block-library/faq/utils.ts
+++ src/blocks/block-library/faq/utils.ts
@@ -1,10 +1,19 @@
+import classnames from 'classnames';
+/**
+ * WordPress dependencies.
+ */
+import { getColorClassName } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import type { BorderTypeOption } from './types';
+
+/**
+ * Block dependencies.
+ */
+import type { FaqBlockAttributes } from '@aktk/blocks/block-library/faq/types';
 
 /**
  * FAQブロックのボーダータイプ選択肢
  */
-export const faqBorderTypes: BorderTypeOption[] = [
+export const faqBorderTypes: { label: string; name: string }[] = [
 	{ label: __( 'なし', 'ystandard-toolbox' ), name: '' },
 	{ label: __( '上下左右', 'ystandard-toolbox' ), name: 'all' },
 	{ label: __( '下のみ', 'ystandard-toolbox' ), name: 'bottom' },
@@ -27,3 +36,58 @@ export const template: [ string, Record< string, any > ][] = [
 		},
 	],
 ];
+
+export function getFaqClassNames( attributes: FaqBlockAttributes ) {
+	const {
+		isAccordion,
+		borderType,
+		borderSize,
+		backgroundColor,
+		customBackgroundColor,
+		borderColor,
+		customBorderColor,
+	} = attributes;
+
+	const backgroundColorClass =
+		getColorClassName( 'background-color', backgroundColor ) || '';
+	const borderColorClass =
+		getColorClassName( 'border-color', borderColor ) || '';
+
+	return classnames( 'ystdtb-faq', {
+		'is-accordion': isAccordion,
+		'has-padding': 'all' === borderType || backgroundColor,
+		'has-background': backgroundColor || customBackgroundColor,
+		[ backgroundColorClass ]: backgroundColorClass,
+		'has-border': borderSize || borderColor || customBorderColor,
+		[ borderColorClass ]: borderColorClass,
+		[ `border-type--${ borderType }` ]: '' !== borderType,
+	} );
+}
+
+export function getFaqStyle( attributes: FaqBlockAttributes ) {
+	const {
+		borderType,
+		borderSize,
+		backgroundColor,
+		customBackgroundColor,
+		borderColor,
+		customBorderColor,
+	} = attributes;
+
+	const backgroundColorClass =
+		getColorClassName( 'background-color', backgroundColor ) || '';
+	const borderColorClass =
+		getColorClassName( 'border-color', borderColor ) || '';
+
+	return {
+		backgroundColor: backgroundColorClass
+			? undefined
+			: customBackgroundColor,
+		borderColor: borderColorClass ? undefined : customBorderColor,
+		borderWidth: 'all' === borderType ? borderSize : undefined,
+		borderBottomWidth:
+			'bottom' === borderType || 'all' === borderType
+				? borderSize
+				: undefined,
+	};
+}
