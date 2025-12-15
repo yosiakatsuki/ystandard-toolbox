a0df318441b4f5218d10f46490cf7638c152ce57
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-12-15 14:40:57 +0900

    dt : 文字色設定追加

--- src/blocks/block-library/description-list-dt/inspector-controls/typography/index.tsx
+++ src/blocks/block-library/description-list-dt/inspector-controls/typography/index.tsx
@@ -11,12 +11,14 @@ import { Panel } from '@aktk/block-components/components/panel';
  * Block dependencies.
  */
 import { FontSize } from './font-size';
+import { Color } from './color';
 
 // @ts-ignore.
 export function Typography( props ): JSX.Element {
 	return (
 		<Panel title={ __( 'テキスト', 'ystandard-toolbox' ) }>
 			<FontSize { ...props } />
+			<Color { ...props } />
 		</Panel>
 	);
 }
