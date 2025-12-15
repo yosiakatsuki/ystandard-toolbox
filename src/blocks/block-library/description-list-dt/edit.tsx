0ab4cf80b62372ca867508682c6f25cdd7d00b7b
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-12-15 10:20:23 +0900

    dt　エンターキー改善

--- src/blocks/block-library/description-list-dt/edit.tsx
+++ src/blocks/block-library/description-list-dt/edit.tsx
@@ -7,6 +7,7 @@ import {
 	// @ts-ignore.
 	__experimentalUseGradient as useGradient,
 } from '@wordpress/block-editor';
+import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 /**
  * Block Dependencies.
@@ -16,9 +17,41 @@ import { getDtBlockClasses, getDtBlockStyles } from './utils';
 import './style-editor.scss';
 
 export default function Edit( props: DtBlockProps ): JSX.Element {
-	const { attributes, setAttributes } = props;
+	const { attributes, setAttributes, clientId } = props;
 	const { text } = attributes;
 	const { gradientClass, gradientValue } = useGradient();
+	const { selectBlock } = useDispatch( 'core/block-editor' );
+
+	// 次のブロックのclientIdを取得
+	const nextBlockClientId = useSelect(
+		( select ) => {
+			const blockEditorStore = select( 'core/block-editor' ) as any;
+			return blockEditorStore.getNextBlockClientId( clientId );
+		},
+		[ clientId ]
+	);
+
+	/**
+	 * Enterキー押下時の処理
+	 * Shift+Enter: 改行（デフォルト動作）
+	 * Enter: 次のブロックへフォーカス移動
+	 *
+	 * @param {Object} event - キーボードイベント
+	 */
+	const handleKeyDown = ( event: any ) => {
+		console.log( { nextBlockClientId } );
+		// Shift+Enterの場合は改行（デフォルト動作）
+		if ( event.key === 'Enter' && event.shiftKey ) {
+			return;
+		}
+
+		// Enterのみの場合は次のブロックへフォーカス移動
+		if ( event.key === 'Enter' && nextBlockClientId ) {
+			event.preventDefault();
+			// 次のブロックを選択
+			selectBlock( nextBlockClientId );
+		}
+	};
 
 	const blockProps = useBlockProps( {
 		className: getDtBlockClasses( {
@@ -38,6 +71,7 @@ export default function Edit( props: DtBlockProps ): JSX.Element {
 				tagName="dt"
 				value={ text || '' }
 				onChange={ ( value ) => setAttributes( { text: value } ) }
+				onKeyDown={ handleKeyDown }
 				identifier="text"
 				placeholder={ __( '説明タイトル', 'ystandard-toolbox' ) }
 				{ ...blockProps }
