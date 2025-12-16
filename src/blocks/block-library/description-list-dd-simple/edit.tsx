376fbc1691608f117462aa58be0cc30fe1205d91
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2025-12-16 11:20:00 +0900

    dt,ddのEnter処理廃止

--- src/blocks/block-library/description-list-dd-simple/edit.tsx
+++ src/blocks/block-library/description-list-dd-simple/edit.tsx
@@ -7,7 +7,6 @@ import {
 	// @ts-ignore.
 	__experimentalUseGradient as useGradient,
 } from '@wordpress/block-editor';
-import { useDispatch, useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -19,37 +18,9 @@ import './style-editor.scss';
 import { InspectorControls } from '@aktk/blocks/block-library/description-list-dd-simple/inspector-controls';
 
 export default function Edit( props: DdSimpleBlockProps ): JSX.Element {
-	const { attributes, setAttributes, clientId } = props;
+	const { attributes, setAttributes } = props;
 	const { text } = attributes;
 	const { gradientClass, gradientValue, setGradient } = useGradient();
-	const { selectBlock } = useDispatch( 'core/block-editor' );
-
-	// 次のブロックのclientIdを取得
-	const nextBlockClientId = useSelect(
-		( select ) => {
-			const blockEditorStore = select( 'core/block-editor' ) as any;
-			return blockEditorStore.getNextBlockClientId( clientId );
-		},
-		[ clientId ]
-	);
-
-	/**
-	 * Enterキー押下時の処理
-	 * Shift+Enter: 改行（デフォルト動作）
-	 * Enter: 次のブロックへフォーカス移動
-	 *
-	 * @param {Object} event - キーボードイベント
-	 */
-	const handleKeyDown = ( event: any ) => {
-		// Enterのみの場合は次のブロックへフォーカス移動
-		if ( event.key === 'Enter' && ! event.shiftKey ) {
-			event.preventDefault();
-			// 次のブロックがあれば次を選択
-			if ( nextBlockClientId ) {
-				selectBlock( nextBlockClientId );
-			}
-		}
-	};
 
 	const blockProps = useBlockProps( {
 		className: getDdSimpleBlockClasses( {
@@ -77,7 +48,6 @@ export default function Edit( props: DdSimpleBlockProps ): JSX.Element {
 				tagName="dd"
 				value={ text || '' }
 				onChange={ ( value ) => setAttributes( { text: value } ) }
-				onKeyDown={ handleKeyDown }
 				identifier="text"
 				placeholder={ __( '説明文', 'ystandard-toolbox' ) }
 				{ ...blockProps }
