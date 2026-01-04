ede012edacb2b3b8b999d776cbb837971836f3b3
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-01-04 22:58:36 +0900

    banner-linkのCustomSizeControl対応 (#106)
    * banner-link：max-width - CustomSizeControl対応
    
    * banner-link：max-height - CustomSizeControl対応
    
    * banner-link：subTextMargin - CustomSizeControl対応

--- src/blocks/block-library/banner-link/inspector-controls/sub-text/margin.tsx
+++ src/blocks/block-library/banner-link/inspector-controls/sub-text/margin.tsx
@@ -8,22 +8,50 @@ import { __ } from '@wordpress/i18n';
  */
 import BaseControl from '@aktk/block-components/wp-controls/base-control';
 import { CustomSizeControl } from '@aktk/block-components/components/custom-size-control';
+import { isResponsive } from '@aktk/block-components/utils/object';
 
+// @ts-ignore
 const Margin = ( { attributes, setAttributes } ) => {
 	const { subTextMargin } = attributes;
+
+	// @ts-ignore
 	const handleOnChange = ( value ) => {
+		if ( undefined === value ) {
+			return;
+		}
+		setAttributes( {
+			subTextMargin: {
+				desktop: value,
+			},
+		} );
+	};
+
+	// @ts-ignore
+	const handleOnChangeResponsive = ( value ) => {
+		if ( undefined === value ) {
+			return;
+		}
 		setAttributes( {
 			subTextMargin: value,
 		} );
 	};
+
+	let _value;
+	if ( ! isResponsive( subTextMargin ) ) {
+		_value = subTextMargin?.desktop;
+	}
+
 	return (
 		<BaseControl
 			id="sub-text-margin"
 			label={ __( 'メインテキストとの間隔', 'ystandard-toolbox' ) }
 		>
 			<CustomSizeControl
-				value={ subTextMargin }
+				value={ _value }
+				responsiveValue={ subTextMargin }
 				onChange={ handleOnChange }
+				onChangeResponsive={ handleOnChangeResponsive }
+				showResetButton={ false }
 			/>
 		</BaseControl>
 	);
