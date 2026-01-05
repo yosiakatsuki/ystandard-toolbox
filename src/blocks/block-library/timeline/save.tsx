9748c385ca6f5ef532b69155f7bb0e7327329df8
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-01-05 10:05:05 +0900

    timeline：Save作成

--- src/blocks/block-library/timeline/save.tsx
+++ src/blocks/block-library/timeline/save.tsx
@@ -1,7 +1,24 @@
-export default function Save( props ) {
+/**
+ * WordPress
+ */
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Block dependencies.
+ */
+import type { TimeLineProps } from './types';
+import { getTimelineClasses } from './utils';
+
+export default function Save( props: TimeLineProps ) {
+	// @ts-ignore.
+	const { attributes } = props;
+
+	const blockProps = useBlockProps.save( {
+		className: getTimelineClasses(),
+	} );
 	return (
-		<>
-			<div>Timeline</div>
-		</>
+		<div { ...blockProps }>
+			<InnerBlocks.Content />
+		</div>
 	);
 }
