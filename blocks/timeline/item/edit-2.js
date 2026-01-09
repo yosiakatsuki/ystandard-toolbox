6b79369b391d27781f5a9ed701c619fe381f88dd
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-01-09 11:45:29 +0900

    timeline-item：edit HTML部分作成

---
+++ blocks/timeline/item/edit-2.js
@@ -0,0 +1,408 @@
+import classnames from 'classnames';
+import { labelTypes, innerMargin, template } from './config';
+import {
+	InnerBlocks,
+	InspectorControls,
+	withColors,
+	FontSizePicker,
+	withFontSizes,
+	PlainText,
+	useBlockProps,
+} from '@wordpress/block-editor';
+import {
+	PanelBody,
+	BaseControl,
+	RangeControl,
+	Button,
+	ToggleControl,
+	ColorPalette,
+} from '@wordpress/components';
+import { withSelect, select } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
+import { __ } from '@wordpress/i18n';
+import SVGIconSelect from '@ystd/components/icon-picker';
+import SVGIcon from '@ystd/components/svg-icon';
+import { calcContentMarginTop } from './function';
+
+function TimelineItem( props ) {
+	const {
+		attributes,
+		setAttributes,
+		labelColor,
+		setLabelColor,
+		labelBackgroundColor,
+		setLabelBackgroundColor,
+		labelBorderColor,
+		setLabelBorderColor,
+		contentsBorderColor,
+		setContentsBorderColor,
+		labelFontSize,
+		setLabelFontSize,
+		hasChildBlocks,
+		className,
+	} = props;
+	const {
+		labelType,
+		labelContents,
+		labelBold,
+		labelBorderSize,
+		labelBorderRadius,
+		contentMarginTop,
+		contentsInnerMargin,
+	} = attributes;
+
+	return (
+		<>
+			<InspectorControls>
+				<PanelBody title={ __( 'タイムライン', 'ystandard-toolbox' ) }>
+					<BaseControl
+						id={ 'contents-margin' }
+						label={ __(
+							'コンテンツ内の余白',
+							'ystandard-toolbox'
+						) }
+						__nextHasNoMarginBottom
+					>
+						<div className="ystdtb__horizon-buttons">
+							{ innerMargin.map( ( item ) => {
+								return (
+									<Button
+										key={ item.name }
+										isSecondary={
+											selectContentsInnerMargin !==
+											item.name
+										}
+										isPrimary={
+											selectContentsInnerMargin ===
+											item.name
+										}
+										onClick={ () => {
+											setAttributes( {
+												contentsInnerMargin: item.name,
+											} );
+										} }
+									>
+										<span>{ item.label }</span>
+									</Button>
+								);
+							} ) }
+						</div>
+					</BaseControl>
+					<BaseControl
+						id={ 'contents-border-color' }
+						label={ __( '線の色', 'ystandard-toolbox' ) }
+						__nextHasNoMarginBottom
+					>
+						<ColorPalette
+							colors={ colors }
+							disableCustomColors={ false }
+							onChange={ ( color ) => {
+								setContentsBorderColor( color );
+							} }
+							value={ contentsBorderColor.color }
+						/>
+					</BaseControl>
+					<BaseControl
+						id={ 'contents-border-color' }
+						label={ __(
+							'コンテンツ上部余白',
+							'ystandard-toolbox'
+						) }
+						__nextHasNoMarginBottom
+					>
+						<RangeControl
+							value={ () => {
+								return undefined === contentMarginTop
+									? 0
+									: contentMarginTop;
+							} }
+							onChange={ ( value ) =>
+								setAttributes( { contentMarginTop: value } )
+							}
+							initialPosition={
+								undefined === contentMarginTop
+									? 0
+									: contentMarginTop
+							}
+							min={ -100 }
+							max={ 100 }
+							step={ 1 }
+							help={ __(
+								'ラベルにアイコンやテキストを使ったときにコンテンツの開始位置がずれる場合、余白を調整してください。',
+								'ystandard-toolbox'
+							) }
+							allowReset={ true }
+							__next40pxDefaultSize
+							__nextHasNoMarginBottom
+						/>
+					</BaseControl>
+				</PanelBody>
+				<PanelBody
+					title={ __( 'ラベル', 'ystandard-toolbox' ) }
+					initialOpen={ false }
+				>
+					<BaseControl
+						id={ 'label-contents' }
+						label={ __( '角丸', 'ystandard-toolbox' ) }
+						__nextHasNoMarginBottom
+					>
+						<RangeControl
+							value={ () => {
+								return undefined === labelBorderRadius
+									? 50
+									: labelBorderRadius;
+							} }
+							onChange={ ( value ) =>
+								setAttributes( { labelBorderRadius: value } )
+							}
+							initialPosition={
+								undefined === labelBorderRadius
+									? 50
+									: labelBorderRadius
+							}
+							min={ 0 }
+							max={ 100 }
+							step={ 1 }
+							allowReset={ true }
+							__next40pxDefaultSize
+							__nextHasNoMarginBottom
+						/>
+					</BaseControl>
+					<BaseControl
+						id={ 'label-contents' }
+						label={ __( 'タイプ', 'ystandard-toolbox' ) }
+						__nextHasNoMarginBottom
+					>
+						<div className="ystdtb__horizon-buttons">
+							{ labelTypes.map( ( item ) => {
+								return (
+									<Button
+										key={ item.name }
+										isSecondary={
+											selectLabelType !== item.name
+										}
+										isPrimary={
+											selectLabelType === item.name
+										}
+										onClick={ () => {
+											const contents =
+												'icon' === item.name &&
+												selectedLabelType !== item.name
+													? 'bookmark'
+													: '';
+											setAttributes( {
+												labelType: item.name,
+												labelContents: contents,
+											} );
+										} }
+									>
+										<span>{ item.label }</span>
+									</Button>
+								);
+							} ) }
+						</div>
+					</BaseControl>
+					{ 'icon' === selectLabelType && (
+						<BaseControl
+							id={ 'label-contents-icon' }
+							label={ __( 'アイコン', 'ystandard-toolbox' ) }
+							__nextHasNoMarginBottom
+						>
+							<SVGIconSelect
+								panelTitle={ __(
+									'アイコン選択',
+									'ystandard-toolbox'
+								) }
+								iconControlTitle={ '' }
+								selectedIcon={ labelContents }
+								onClickIcon={ ( value ) => {
+									setAttributes( { labelContents: value } );
+								} }
+							/>
+						</BaseControl>
+					) }
+					{ 'text' === selectLabelType && (
+						<>
+							<BaseControl
+								id={ 'label-contents-text' }
+								label={ __( 'テキスト', 'ystandard-toolbox' ) }
+								__nextHasNoMarginBottom
+							>
+								<PlainText
+									value={ labelContents }
+									style={ {
+										border: '1px solid #ccc',
+										padding: '.25em',
+									} }
+									onChange={ ( value ) => {
+										setAttributes( {
+											labelContents: value,
+										} );
+									} }
+									placeholder={ __(
+										'テキスト',
+										'ystandard-toolbox'
+									) }
+									aria-label={ __(
+										'テキスト',
+										'ystandard-toolbox'
+									) }
+								/>
+							</BaseControl>
+							<BaseControl
+								id={ 'label-contents-weight' }
+								label={ __(
+									'文字の太さ',
+									'ystandard-toolbox'
+								) }
+								__nextHasNoMarginBottom
+							>
+								<ToggleControl
+									label={ __(
+										'太字にする',
+										'ystandard-toolbox'
+									) }
+									onChange={ () => {
+										setAttributes( {
+											labelBold: ! labelBold,
+										} );
+									} }
+									checked={ labelBold }
+								/>
+							</BaseControl>
+						</>
+					) }
+
+					{ '' !== selectLabelType && (
+						<>
+							<BaseControl
+								id={ 'label-contents-size' }
+								label={ __(
+									'文字・アイコン サイズ',
+									'ystandard-toolbox'
+								) }
+								__nextHasNoMarginBottom
+							>
+								<FontSizePicker
+									label={ __(
+										'サイズ',
+										'ystandard-toolbox'
+									) }
+									value={ labelFontSize.size }
+									onChange={ ( font ) => {
+										setLabelFontSize( font );
+									} }
+									__nextHasNoMarginBottom
+									__next40pxDefaultSize
+								/>
+							</BaseControl>
+							<BaseControl
+								id={ 'label-contents-color' }
+								label={ __(
+									'文字・アイコン 色',
+									'ystandard-toolbox'
+								) }
+								__nextHasNoMarginBottom
+							>
+								<ColorPalette
+									colors={ colors }
+									disableCustomColors={ false }
+									onChange={ ( color ) => {
+										setLabelColor( color );
+									} }
+									value={ labelColor.color }
+								/>
+							</BaseControl>
+						</>
+					) }
+					<BaseControl
+						id={ 'label-bg' }
+						label={ __( '背景色', 'ystandard-toolbox' ) }
+						__nextHasNoMarginBottom
+					>
+						<ColorPalette
+							colors={ colors }
+							disableCustomColors={ false }
+							onChange={ ( color ) => {
+								setLabelBackgroundColor( color );
+							} }
+							value={ labelBackgroundColor.color }
+						/>
+					</BaseControl>
+					<BaseControl
+						id={ 'label-border' }
+						label={ __( '枠線の太さ', 'ystandard-toolbox' ) }
+						__nextHasNoMarginBottom
+					>
+						<RangeControl
+							value={ labelBorderSize }
+							onChange={ ( value ) =>
+								setAttributes( { labelBorderSize: value } )
+							}
+							initialPosition={
+								undefined === labelBorderSize
+									? 0
+									: labelBorderSize
+							}
+							min={ 0 }
+							max={ 100 }
+							step={ 1 }
+							allowReset={ true }
+							__next40pxDefaultSize
+							__nextHasNoMarginBottom
+						/>
+					</BaseControl>
+					<BaseControl
+						id={ 'label-border-color' }
+						label={ __( '枠線の色', 'ystandard-toolbox' ) }
+						__nextHasNoMarginBottom
+					>
+						<ColorPalette
+							colors={ colors }
+							disableCustomColors={ false }
+							onChange={ ( color ) => {
+								setLabelBorderColor( color );
+							} }
+							value={ labelBorderColor.color }
+						/>
+					</BaseControl>
+				</PanelBody>
+			</InspectorControls>
+
+			<div { ...blockProps }>
+				<div className={ labelClasses } style={ labelStyles }>
+					{ getLabelContents() }
+				</div>
+				<div className={ contentsClass } style={ contentsStyle }>
+					<InnerBlocks
+						templateLock={ false }
+						template={ template }
+						renderAppender={
+							hasChildBlocks
+								? undefined
+								: () => <InnerBlocks.ButtonBlockAppender />
+						}
+					/>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default compose( [
+	withColors( {
+		labelColor: 'color',
+		labelBackgroundColor: 'backgroundColor',
+		labelBorderColor: 'borderColor',
+		contentsBorderColor: 'borderColor',
+	} ),
+	withFontSizes( 'labelFontSize' ),
+	withSelect( ( ownSelect, ownProps ) => {
+		const { clientId } = ownProps;
+		const { getBlockOrder } = ownSelect( 'core/block-editor' );
+
+		return {
+			hasChildBlocks: getBlockOrder( clientId ).length > 0,
+		};
+	} ),
+] )( TimelineItem );
--- src/blocks/block-library/timeline-item/edit.tsx
+++ src/blocks/block-library/timeline-item/edit.tsx
@@ -1,15 +1,81 @@
 /**
+ * WordPress
+ */
+import { __ } from '@wordpress/i18n';
+import { useInnerBlocksProps, useBlockProps } from '@wordpress/block-editor';
+/**
+ * Aktk Dependencies.
+ */
+import { SvgIcon } from '@aktk/block-components/components/svg-icon';
+/**
  * Block dependencies.
  */
 import { InspectorControls } from './inspector-controls';
 import type { TimeLineItemProps } from './types';
 import './style-editor.scss';
+import {
+	getLabelClasses,
+	getLabelContentClasses,
+	getLabelStyles,
+	getTimelineContentClasses,
+	getTimelineContentStyles,
+	getTimelineItemClasses,
+	getTimelineItemStyles,
+} from '@aktk/blocks/block-library/timeline-item/utils';
+
+const TEMPLATE = [
+	[
+		'core/paragraph',
+		{ placeholder: __( 'タイムラインコンテンツ…', 'ystandard-toolbox' ) },
+	],
+];
 
 export default function Edit( props: TimeLineItemProps ) {
+	const { attributes } = props;
+
+	const { labelType, labelContents } = attributes;
+
+	const _labelType = labelType || '';
+
+	const blockProps = useBlockProps( {
+		className: getTimelineItemClasses( attributes ),
+		style: getTimelineItemStyles( attributes ),
+	} );
+
+	const labelClasses = getLabelClasses( attributes );
+	const labelStyles = getLabelStyles( attributes );
+
+	const labelContentsClasses = getLabelContentClasses( attributes );
+
+	const contentsClass = getTimelineContentClasses( attributes );
+	const contentsStyle = getTimelineContentStyles( attributes );
+
+	// InnerBlocks Props.
+	const innerBlocksProps = useInnerBlocksProps(
+		{
+			className: contentsClass,
+			style: contentsStyle,
+		},
+		{
+			// @ts-ignore
+			template: TEMPLATE,
+		}
+	);
+
 	return (
 		<>
 			<InspectorControls { ...props } />
-			<div>タイムラインItem</div>
+			<div { ...blockProps }>
+				<div className={ labelClasses } style={ labelStyles }>
+					<span className={ labelContentsClasses }>
+						{ 'text' === _labelType && <>{ labelContents }</> }
+						{ 'icon' === _labelType && (
+							<SvgIcon name={ labelContents } />
+						) }
+					</span>
+				</div>
+				<div { ...innerBlocksProps } />
+			</div>
 		</>
 	);
 }
