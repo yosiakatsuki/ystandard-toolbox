2433fe06f37fe535a38f641c5ac6d4a46319a280
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-01-14 22:32:55 +0900

    Timeline旧ファイル削除

--- blocks/timeline/config.js
+++
@@ -1,7 +0,0 @@
-import { __ } from '@wordpress/i18n';
-
-export const presetLabelTypes = [
-	{ label: __( 'なし', 'ystandard-toolbox' ), name: '' },
-	{ label: __( 'アイコン', 'ystandard-toolbox' ), name: 'icon' },
-	{ label: __( '連番', 'ystandard-toolbox' ), name: 'text' },
-];
