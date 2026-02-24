2a02ca200097e2a49ef795cc3f29017e605aaad3
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-02-24 17:33:46 +0900

    Posts：型作成

--- src/blocks/block-library/posts/edit.tsx
+++ src/blocks/block-library/posts/edit.tsx
@@ -1,6 +1,10 @@
+/**
+ * Block dependencies.
+ */
+import type { PostsEditProps } from './types';
 import './style-editor.scss';
 
-export default function Edit( props ) {
+export default function Edit( props: PostsEditProps ) {
 	return (
 		<>
 			<div>Edit</div>
