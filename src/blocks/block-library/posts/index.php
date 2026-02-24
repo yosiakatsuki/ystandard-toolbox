8206586323bb06b5926a8aced5979c5299ca1844
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-02-24 17:42:31 +0900

    Posts：index.php作成

--- src/blocks/block-library/posts/index.php
+++ src/blocks/block-library/posts/index.php
@@ -1,1 +1,80 @@
+<?php
+/**
+ * 投稿一覧ブロック
+ *
+ * @package ystandard-toolbox
+ */
 
+namespace ystandard_toolbox;
+
+defined( 'ABSPATH' ) || die();
+
+
+class Posts_Block {
+
+	const BLOCK_NAME = 'ystdtb/posts';
+
+	/**
+	 * Instance.
+	 *
+	 * @var Sns_Share_Block
+	 */
+	private static $instance;
+
+	/**
+	 * ブロック属性
+	 *
+	 * @var array
+	 */
+	private static $attributes;
+
+	/**
+	 * Constructor.
+	 */
+	private function __construct() {
+		add_action( 'init', [ $this, 'register_block' ], 100 );
+	}
+
+	/**
+	 * Instance.
+	 *
+	 * @return Sns_Share_Block
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * ブロック登録
+	 *
+	 * @return void
+	 */
+	public function register_block() {
+		register_block_type(
+			__DIR__,
+			[
+				'render_callback' => [ $this, 'render_callback' ],
+			]
+		);
+	}
+
+	/**
+	 * レンダーコールバック
+	 *
+	 * @param array $attributes ブロック属性.
+	 *
+	 * @return string
+	 */
+	public function render_callback( $attributes ) {
+
+		$html = 'postsブロック';
+
+		return $html;
+	}
+}
+
+Posts_Block::get_instance();
