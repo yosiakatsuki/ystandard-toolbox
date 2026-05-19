f2795ee5cfb642fb0c8271b9279d474b34b4ed50
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-05-19 18:05:15 +0900

    banner-link Block Bindings対応

--- src/blocks/block-library/banner-link/block-controls/index.tsx
+++ src/blocks/block-library/banner-link/block-controls/index.tsx
@@ -1,9 +1,15 @@
-import { BlockControls } from '@wordpress/block-editor';
+import { BlockControls, useBlockEditingMode } from '@wordpress/block-editor';
 import LinkButton from './link-button';
 import ContentPosition from './content-position';
 import BlockPosition from './block-position';
 
 export const BannerLinkBlockControls = ( props ) => {
+	const blockEditingMode = useBlockEditingMode();
+
+	if ( 'contentOnly' === blockEditingMode ) {
+		return null;
+	}
+
 	return (
 		<BlockControls>
 			<LinkButton { ...props } />
