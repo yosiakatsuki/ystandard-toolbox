1189944e9b9b90aa5a49c1e89eee813f9261d1d3
Yoshiaki Ogata <y.ogata@yosiakatsuki.net>
2026-05-26 10:07:32 +0900

    banner-linkブロックのResponsiveSpacingSelect調整

--- src/blocks/block-library/banner-link/inspector-controls/padding/padding.tsx
+++ src/blocks/block-library/banner-link/inspector-controls/padding/padding.tsx
@@ -11,51 +11,31 @@ import {
 	ResponsiveSpacingSelect,
 	type ResponsiveSpacing,
 	type ResponsiveSpacingSelectOnChangeProps,
-	type Spacing,
 } from '@aktk/block-components/components/custom-spacing-select';
 import { stripUndefined } from '@aktk/block-components/utils/object';
 
-const hasResponsiveSpacing = ( value?: ResponsiveSpacing ) => {
-	return !! value?.tablet || !! value?.mobile;
-};
 
-const getDefaultSpacing = (
-	value?: ResponsiveSpacing
-): Spacing | undefined => {
-	return value?.desktop;
-};
-
-const getResponsiveSpacing = (
-	value?: ResponsiveSpacing
-): ResponsiveSpacing | undefined => {
-	if ( ! hasResponsiveSpacing( value ) ) {
-		return undefined;
-	}
-	return value;
-};
-
-const toLegacyPadding = (
-	value: ResponsiveSpacingSelectOnChangeProps
-): ResponsiveSpacing | undefined => {
-	if ( value.responsiveSpacing ) {
-		return stripUndefined( value.responsiveSpacing ) as ResponsiveSpacing;
-	}
-	if ( value.spacing ) {
-		return stripUndefined( {
-			desktop: value.spacing,
-		} ) as ResponsiveSpacing;
-	}
-	return undefined;
-};
-
+// @ts-ignore
 const Padding = ( props ) => {
 	const { attributes, setAttributes } = props;
 
 	const padding = attributes.padding as ResponsiveSpacing | undefined;
 
 	const handleOnChange = ( values: ResponsiveSpacingSelectOnChangeProps ) => {
+
+		let newPadding : ResponsiveSpacing | undefined;
+		if ( values.responsiveSpacing ) {
+			newPadding = stripUndefined( values.responsiveSpacing ) as ResponsiveSpacing;
+		} else if ( values.spacing ) {
+			newPadding = stripUndefined( {
+				desktop: values.spacing,
+			} ) as ResponsiveSpacing;
+		} else {
+			newPadding = undefined;
+		}
+
 		setAttributes( {
-			padding: toLegacyPadding( values ),
+			padding: newPadding,
 		} );
 	};
 
@@ -65,8 +45,8 @@ const Padding = ( props ) => {
 			label={ __( '内側余白', 'ystandard-toolbox' ) }
 		>
 			<ResponsiveSpacingSelect
-				value={ getDefaultSpacing( padding ) }
-				responsiveValue={ getResponsiveSpacing( padding ) }
+				value={ padding?.desktop }
+				responsiveValue={ padding }
 				onChange={ handleOnChange }
 			/>
 		</BaseControl>
